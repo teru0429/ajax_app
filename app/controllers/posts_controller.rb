87f4e9741d5ab0ab1333c128cd09742565167b76
teru0429 <75606622+teru0429@users.noreply.github.com>
2020-12-18 21:25:50 +0900

    既読機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,20 @@
 class PostsController < ApplicationController
-  def index  # indexアクションを定義した
-    @posts = Post.all.order(id: "DESC")  # すべてのレコードを@postsに代入
+  def index
+    @posts = Post.all.order(id: "DESC")
   end
-
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
   end
-
   def checked
-    post = post.find(params[:id])
+    post = Post.find(params[:id])
     if post.checked
       post.update(checked: false)
     else
       post.update(checked: true)
     end
-
-    item = post.find(params[:id])
-    render json: { post:item}
+    item = Post.find(params[:id])
+    render json: { post: item }
   end
+
 end
