d3b84d036b8a549472fbe34a5129169bbeab5856
teru0429 <75606622+teru0429@users.noreply.github.com>
2020-12-20 12:47:35 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == EMV["BASIC_AUTH_USER"] && password == EMV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
